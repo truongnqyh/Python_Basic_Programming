acdd99d01cd06f537bb5877d2fe2575ea155f96c
TruongKom <truongnqyh@gmail.com>
2025-07-13 15:32:18 +0700

    Day 1

---
+++ Python Basic/Test2.py
@@ -0,0 +1,19 @@
+#### DATA TYPES - DEFINE VARIABLE AND
+integer_var = 3
+float_var = 4.2
+string_var = "TRUONG"
+list_var = [1, 2, 3]
+tuple_var = [4, 5, 6]
+dict_var = {"name": "Alice", "role": "Engineer"}
+bool_var = True
+
+#Print and manipulate
+print("Integer: ", integer_var)
+print("Float: ", float_var)
+print("String: ", string_var + " is learning ML_DL")
+list_var.append(4)
+print("List: ", list_var)
+print("Tuple: ", tuple_var)
+print("Dict : ", dict_var)
+print("Boolean: ", bool_var)
+
